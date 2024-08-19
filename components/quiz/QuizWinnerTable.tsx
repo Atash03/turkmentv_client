45b57d9599bed58efa974c129bd3424cff56ae2a
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-08-19 17:58:58 +0500

    clg added

--- components/quiz/QuizWinnerTable.tsx
+++ components/quiz/QuizWinnerTable.tsx
@@ -16,6 +16,8 @@ const QuizWinnerTable = ({ quizId, quizFinished }: IProps) => {
   const [winnersData, setWinnersData] = useState<IQuizQuestionsWinners>();
   const { questionsData } = useContext(QuizContext).quizQuestionsContext;
 
+  console.log('first');
+
   useEffect(() => {
     if (quizFinished) {
       // Queries.getQuizQuestions().then((res) => {
