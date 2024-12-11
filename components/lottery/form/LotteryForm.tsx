f28671f99e0a03c5bf111da35d4cf2f12efd12ee
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-12-11 18:14:29 +0500

    LotteryForm added

---
+++ components/lottery/form/LotteryForm.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const LotteryForm = () => {
+  const [input, setInput] = useState<string>('');
+
+  return (
+    <div className="p-6 flex flex-col gap-3">
+      <div className="flex flex-col gap-2">
+        <span className="font-base-medium">Siziň kodyňyz</span>
+        <div className="flex flex-col w-full gap-1">
+          <input
+            type="text"
+            className="p-4 rounded-xl bg-lightSurfaceContainerHigher font-base-regular text-lightOnSurfaceVariant placeholder:text-lightOnSurfaceVariant"
+            placeholder="123456"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <span className="font-xsmall-regular">Öz kodyňyzy giriziň</span>
+        </div>
+      </div>
+
+      <button className="p-[10px] w-full flex items-center justify-center text-lightPrimary font-small-medium rounded-full outline-none border border-lightOutline hover:bg-[#57599214] transition-all ease-in-out duration-300">
+        Tassyklamak
+      </button>
+    </div>
+  );
+};
+
+export default LotteryForm;
